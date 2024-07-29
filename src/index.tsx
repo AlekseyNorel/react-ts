7b61fcfc75f71836676973f8809ff4799dbe67d2
AlekseyNorel <aleksei.julivi@gmail.com>
2024-07-29 16:37:56 +0300

    q

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,11 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import axios from 'axios';
+import {RouterProvider} from "react-router-dom";
+import { router } from './router';
 
 
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
-
-let router = createBrowserRouter([]);
-
-
 
 root.render(<RouterProvider router = {router} />);
